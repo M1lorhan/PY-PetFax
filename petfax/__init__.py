4870b901dc1c7236ec74d126768b8e740ec11026
Marshall Lorhan <mlorhan@gmail.com>
2024-06-24 20:09:05 -0700

    added templates and database migrations

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask 
 from . import pet
+import os
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    from . import models
+    models.db.init_app(app)
+
+    migrate = Migrate(app, models.db)
+    
 
     @app.route('/')
     def hello(): 
@@ -10,6 +21,9 @@ def create_app():
         
     # Register Blueprints
     app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     # Return App
     return app
--- petfax/pet.py
+++ petfax/pet.py
@@ -13,14 +13,10 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 # Pets Index
 @bp.route('/')
 def index():
-    return render_template('index.html', pets=pets_data)
+    return render_template('pets/index.html', pets=pets_data)
 
 @bp.route('/<int:pet_id>')
 def show_pet(pet_id):
     for pet in pets_data:
         if pet['pet_id'] == pet_id:
-            return render_template('show.html', pet=pet)
-                                   
-@bp.route("/facts/new")
-def submit_fact():
-    return render_template('new.html')
+            return render_template('pets/show.html', pet=pet)
